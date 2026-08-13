a0a6897121cc53164401e11853573871e80e37c9
anntrofim-debug <ann.trofim@gmail.com>
2026-08-13 14:52:56 +0200

    Temporary delete

--- pizzeria_frontend/src/context/useAuthContext.jsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "./AuthContext";
-
-// Lille hjælpe-hook, så komponenter kan skrive useAuthContext() i stedet for
-// useContext(AuthContext). Giver adgang til token, user, login og logout.
-export const useAuthContext = () => useContext(AuthContext);
